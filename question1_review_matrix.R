f784e861f3bf021d54cb82a3e712e595bd001cb4
Sarah Chen <sarahchen0401@163.com>
2020-03-07 15:33:01 +0800

    MCM script

--- question1_review_matrix.R
+++ question1_review_matrix.R
@@ -1,54 +1,32 @@
+#####question1:convert reviews to matrix#######
 setwd("E:\\MCM\\processed_data")
 microwave.head.length <- read.table("microwave\\microwave.head.length.txt",header = F,as.is=T)
-microwave.head.length <- as.matrix(unlist(microwave.head.length))
+microwave.head.length <- as.matrix(unlist(microwave.head.length)) ##review's length
 microwave.head_unique_words <- read.table("microwave\\microwave.head_unique_words.txt",as.is = T,header = F)
-microwave.head_unique_words <- as.character(unlist(microwave.head_unique_words))
+microwave.head_unique_words <- as.character(unlist(microwave.head_unique_words))#unique words
 microwave.count.row <- read.table("microwave\\microwave.head.count_row.txt",header = F,as.is = T)
-microwave.count.row <- microwave.count.row +1
-filelist = list.files(path = "microwave\\",pattern = "microwave.head.[0-9].*.word_count.txt")
+microwave.count.row <- as.data.frame(microwave.count.row +1) ##the number of occourance of unique words
+microwave.count.row$word <-  microwave.head_unique_words
+filelist = list.files(path = "microwave\\",pattern = "microwave.head.[0-9].*.word_count.txt")#the number of each word in one review
 #assuming tab separated values with a header    
-datalist = lapply(filelist, function(x)read.table(paste0("microwave\\",x), header=F,fill =T )) 
+datalist = lapply(filelist, function(x)read.table(paste0("microwave\\",x), header=F,fill =T,as.is=T)) 
 #assuming the same header/columns for all files
 #datafr = do.call("rbind", datali(st) 
-reviwe_head <- matrix(0,nrow = dim(microwave.head.length)[1],ncol = length(microwave.head_unique_words))
-colnames(reviwe_head) <- microwave.head_unique_words
+TF <- matrix(0,nrow = dim(microwave.head.length)[1],ncol = length(microwave.head_unique_words))
+colnames(TF) <- microwave.head_unique_words
+library(stringr)
 for (i in 1:length(filelist)) {
-  ind <- match(as.character(unlist(datalist[[i]][2])),microwave.head_unique_words)
+  data <- matrix(unlist(datalist[[i]]),ncol = 2, byrow = F)#convert list to matrix
+  name <- filelist[i]
+  number <- as.numeric(str_extract( name,'[0-9]+'))#extract the file ture number
+  ind <- match(as.character(data[,2]),microwave.head_unique_words)
   non_na <- !is.na(ind)
-  reviwe_head[i,na.omit(ind)] <- as.data.frame(datalist[[i]])[non_na,1]/microwave.head.length[i,1]
+  TF[number,na.omit(ind)]  <- as.numeric(data[non_na,1])/microwave.head.length[number,1]
 }
 
-idf <- log10(length(filelist)/microwave.count.row)
-TD_IDF_microwave_head <- t(apply(as.matrix(reviwe_head), 1, function(x) as.matrix(x*idf)))
-colnames(TD_IDF_microwave_head) <- colnames(reviwe_head)
-
-
-
-microwave.head.length <- read.table("hair_dryer\\hair_dryer.head.length.txt",header = F,as.is=T)
-microwave.head.length <- as.matrix(unlist(microwave.head.length))
-microwave.head_unique_words <- read.table("hair_dryer\\hair_dryer.head_unique_words.txt",as.is = T,header = F)
-microwave.head_unique_words <- as.character(unlist(microwave.head_unique_words))
-microwave.count.row <- read.table("hair_dryer\\hair_dryer.head.count_row.txt",header = F,as.is = T)
-microwave.count.row <- microwave.count.row +1
-filelist = list.files(path = "hair_dryer\\",pattern = "hair_dryer.head.[0-9].*.word_count.txt")
-#assuming tab separated values with a header    
-datalist = lapply(filelist, function(x)read.table(paste0("hair_dryer\\",x), header=F,fill =T )) 
-#assuming the same header/columns for all files
-#datafr = do.call("rbind", datali(st) 
-reviwe_head <- matrix(0,nrow = dim(microwave.head.length)[1],ncol = length(microwave.head_unique_words))
-colnames(reviwe_head) <- microwave.head_unique_words
-#for (i in 1:length(filelist)) {
-for (i in 1:dim(microwave.head.length)[1]) { 
- ind <- match(as.character(unlist(datalist[[i]][2])),microwave.head_unique_words)
-  non_na <- !is.na(ind)
-  reviwe_head[i,na.omit(ind)] <- as.data.frame(datalist[[i]])[non_na,1]/microwave.head.length[i,1]
-}
-
-idf <- log10(length(filelist)/microwave.count.row)
-TD_IDF_microwave_head <- t(apply(as.matrix(reviwe_head), 1, function(x) as.matrix(x*idf)))
-colnames(TD_IDF_microwave_head) <- colnames(reviwe_head)
-
-
+IDF <- log10(length(filelist)/microwave.count.row[,1])##IDF matrix (number of reviews/number of unique words' occourance in all reviews)
+TF_IDF_microwave_head <- t(apply(as.matrix(TF), 1, function(x) as.matrix(x*IDF)))##TF-IDF matrix
+colnames(TF_IDF_microwave_head) <- colnames(TF)
 
 review_mat <- function(product,reviewPart){
   microwave.head.length <- read.table(paste0(product,"\\",product,".",reviewPart,".length.txt"),header = F,as.is=T)
@@ -56,26 +34,29 @@ review_mat <- function(product,reviewPart){
   microwave.head_unique_words <- read.table(paste0(product,"\\",product,".",reviewPart,"_unique_words.txt"),as.is = T)
   microwave.head_unique_words <- as.character(unlist(microwave.head_unique_words))
   microwave.count.row <- read.table(paste0(product,"\\",product,".",reviewPart,".count_row.txt"),header = F,as.is = T)
-  microwave.count.row <- microwave.count.row +1
+  microwave.count.row <- as.data.frame(microwave.count.row +1) ##the number of occourance of unique words
+  microwave.count.row$word <-  microwave.head_unique_words
   filelist = list.files(path = paste0(product,"\\"),pattern = paste0(product,".",reviewPart,".[0-9].*.word_count.txt"))
   #assuming tab separated values with a header    
-  datalist = lapply(filelist, function(x)read.table(paste0(product,"\\",x), header=F,fill =T )) 
+  datalist = lapply(filelist, function(x)read.table(paste0(product,"\\",x), header=F,fill =T ,as.is=T)) 
   #assuming the same header/columns for all files
   #datafr = do.call("rbind", datali(st) 
-  
-  reviwe_head <- matrix(0,nrow = dim(microwave.head.length)[1],ncol = length(microwave.head_unique_words))
-  colnames(reviwe_head) <- microwave.head_unique_words
+  TF <- matrix(0,nrow = dim(microwave.head.length)[1],ncol = length(microwave.head_unique_words))
+  colnames(TF) <- microwave.head_unique_words
+  library(stringr)
   for (i in 1:length(filelist)) {
-    ind <- match(as.character(unlist(datalist[[i]][2])),microwave.head_unique_words)
+    data <- matrix(unlist(datalist[[i]]),ncol = 2, byrow = F)#convert list to matrix
+    name <- filelist[i]
+    number <- as.numeric(str_extract( name,'[0-9]+'))#extract the file ture number
+    ind <- match(as.character(data[,2]),microwave.head_unique_words)
     non_na <- !is.na(ind)
-    reviwe_head[i,na.omit(ind)] <- as.data.frame(datalist[[i]])[non_na,1]/microwave.head.length[i,1]
+    TF[number,na.omit(ind)]  <- as.numeric(data[non_na,1])/microwave.head.length[number,1]
   }
   
-  idf <- log10(length(filelist)/microwave.count.row)
-  TD_IDF_microwave_head <- t(apply(as.matrix(reviwe_head), 1, function(x) as.matrix(x*idf)))
-  colnames(TD_IDF_microwave_head) <- colnames(reviwe_head)
-  return(TD_IDF_microwave_head)
-  
+  IDF <- log10(length(filelist)/microwave.count.row[,1])##IDF matrix (number of reviews/number of unique words' occourance in all reviews)
+  TF_IDF_microwave_head <- t(apply(as.matrix(TF), 1, function(x) as.matrix(x*IDF)))##TF-IDF matrix
+  colnames(TF_IDF_microwave_head) <- colnames(TF)
+  return(TF_IDF_microwave_head)
 }
 
 
@@ -88,6 +69,9 @@ pacifierBody <- review_mat("pacifier","body")
 
 write.table(microwaveHead,file = "review_mat\\microwaveHead.txt", col.names = T,row.names = F,sep = "\t",quote = F)
 write.table(microwaveBody,file = "review_mat\\microwaveBody.txt", col.names = T,row.names = F,sep = "\t",quote = F)
+write.table(hair_dryerHead,file = "review_mat\\hair_dryerHead.txt", col.names = T,row.names = F,sep = "\t",quote = F)
+write.table(hair_dryerBody,file = "review_mat\\hair_dryerBody.txt", col.names = T,row.names = F,sep = "\t",quote = F)
+write.table(pacifierHead,file = "review_mat\\pacifierHead.txt", col.names = T,row.names = F,sep = "\t",quote = F)
+write.table(pacifierBody,file = "review_mat\\pacifierBody.txt", col.names = T,row.names = F,sep = "\t",quote = F)
 
-#save(microwaveHead,microwaveBody,hair_dryerHead,hair_dryerBody,pacifierHead,pacifierBody, file = "review_mat.Rdata")
-save(microwaveHead,microwaveBody, file = "review_mat.Rdata")
+save(microwaveHead,microwaveBody,hair_dryerHead,hair_dryerBody,pacifierHead,pacifierBody, file = "review_mat\\review_mat.Rdata")
